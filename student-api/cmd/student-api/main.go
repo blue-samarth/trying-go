a7e30f542b5529970ab137037bf3d9baa1737fcf
Samarth <samarth38work@gmail.com>
2025-05-12 09:19:59 +0000

    testing the working of config file

--- student-api/cmd/student-api/main.go
+++ student-api/cmd/student-api/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/blue-samarth/trying-go/student-api/internal/config"
 )
 
 func main(){
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a simple Go program.")
+	cfg := config.MustLoad()
+	fmt.Printf("Loaded config:\n  Env: %s\n  Storage Path: %s\n  HTTP Address: %s\n",
+		cfg.Env, cfg.Storagepath, cfg.HTTPServer.Address)
 }
