d89d1fa84554f9a7944426ca5aebf13e286d6fed
Samarth <samarth38work@gmail.com>
2025-05-12 12:29:04 +0000

    Add the basic router and server

--- student-api/cmd/student-api/main.go
+++ student-api/cmd/student-api/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+
 	"github.com/blue-samarth/trying-go/student-api/internal/config"
 )
 
@@ -11,4 +14,22 @@ func main(){
 	cfg := config.MustLoad()
 	fmt.Printf("Loaded config:\n  Env: %s\n  Storage Path: %s\n  HTTP Address: %s\n",
 		cfg.Env, cfg.Storagepath, cfg.HTTPServer.Address)
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the Student API!"))
+	})
+
+	fmt.Println("Server started on", cfg.HTTPServer.Address)
+	fmt.Println("Server is running...")
+
+	server := http.Server {
+		Addr : cfg.HTTPServer.Address,
+		Handler : router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil { log.Fatalf("Error starting server: %v", err.Error()) }
+
+
 }
