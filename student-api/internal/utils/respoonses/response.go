29c1feb03241c6de22d0e1d5622dbc4eb9740c30
Samarth <samarth38work@gmail.com>
2025-05-18 13:13:16 +0000

    STUDENT-API[feature]-creating a response

---
+++ student-api/internal/utils/respoonses/response.go
@@ -0,0 +1,17 @@
+package responses
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func JsonResponse(w https.ResponseWriter, status string, statusCode int, data interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	return json.NewEncoder(w).Encode(map[string]interface{}{
+		"status":      status,
+		"status_code": statusCode,
+		"data":        data,
+	})
+}
